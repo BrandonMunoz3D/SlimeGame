4414f0bd58ba98ea39c7820a7b1ecbe4eebaeed2
ErinJLaBar <erinlabar@lcad.edu>
2023-05-08 22:49:54 -0700

    fixed projectile
    The player shoots BUT the if statement on thr projectile script that invokes frozen is still returning null. Should have a ranged enemy asap, the one in my spare project is having issues deactivating the projectiles so im sorta redoing the script to find the cause

--- SlimeGameLCAD/Assets/Scripts/HealthBar.cs
+++ SlimeGameLCAD/Assets/Scripts/HealthBar.cs
@@ -5,21 +5,42 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    PlayerHealth healthCurrent;
+    [SerializeField] private GameObject SlimePlayer;
+    PlayerHealth playerHealth;
     [SerializeField] private Image totalHealthBar;
     [SerializeField] private Image currentHealthBar;
-   
+    
+
+    private void Awake()
+    {
+        playerHealth = SlimePlayer.GetComponent<PlayerHealth>();
+    }
 
     private void Start()
     {
-        healthCurrent = GetComponent<PlayerHealth>();
         totalHealthBar.fillAmount = 1;
         currentHealthBar.fillAmount = 1;
     }
 
     private void Update()
     {
-        //currenthealthBar.fillAmount = healthCurrent.currentHealth / 5; 
+        if (SlimePlayer.GetComponent<PlayerHealth>().healthCurrent == 5)
+            currentHealthBar.fillAmount = 1;
+
+        if (SlimePlayer.GetComponent<PlayerHealth>().healthCurrent == 4)
+            currentHealthBar.fillAmount = .8f;
+
+        if (SlimePlayer.GetComponent<PlayerHealth>().healthCurrent == 3)
+            currentHealthBar.fillAmount = .6f;
+
+        if (SlimePlayer.GetComponent<PlayerHealth>().healthCurrent == 2)
+            currentHealthBar.fillAmount = .4f;
+
+        if (SlimePlayer.GetComponent<PlayerHealth>().healthCurrent == 1)
+            currentHealthBar.fillAmount = .2f;
+
+        if (SlimePlayer.GetComponent<PlayerHealth>().healthCurrent == 0)
+            currentHealthBar.fillAmount = 0;
     }
 }
 
